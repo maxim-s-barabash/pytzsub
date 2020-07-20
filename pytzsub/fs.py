5a335e78d83db44652debbcc8bc919806ea65714
maxim-s-barabash <maxim.s.barabash@gmail.com>
2020-07-20 22:28:39 +0300

    refactoring

--- pytzsub/fs.py
+++ pytzsub/fs.py
@@ -1,11 +1,60 @@
-def _read_from(filename, sep='\t'):
+# MIT License
+
+# Copyright (c) 2020 Maxim Barabash
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Implements methods for working with the file system."""
+
+TAB_CHAR = "\t"
+
+
+def read_tab(filename, sep=TAB_CHAR):
+    """Read tab files.
+
+    Description of the teb file structure:
+    - Columns are separated by a single tab.
+    - Lines beginning with '#' are comments.
+    - All text uses UTF-8 encoding.
+
+    The columns of the table are as follows:
+    1.  Subdivision names are listed as in the ISO 3166-2 standard
+        published by the ISO 3166 Maintenance Agency (ISO 3166/MA).
+    2.  Timezone name used in value of TZ environment variable.
+
+    Args:
+        filename (str): the path to the tab file.
+        sep (str, optional): [description]. Defaults to TAB_CHAR.
+
+    Returns:
+        dict: key (str) iso_code, value (dict)
+
+    Example:
+        >>> from pytzsub.sub import DB_FILENAME
+        >>> db = read_tab(DB_FILENAME)
+    """
     table = {}
-    with open(filename, 'r') as f:
-        for line in f:
-            if not line or line.startswith("#"):
-                continue
+    with open(filename, "r") as fileptr:
+        for line in fileptr.readlines():
             line = line.strip()
-            codes = line.split(sep)
-            iso_code = codes[0]
-            table[iso_code] = dict(tz=codes[1])
+            if line and not line.startswith("#"):
+                codes = line.split(sep)
+                iso_code = codes[0]
+                table[iso_code] = dict(tz=codes[1])
     return table
--- pytzsub/sub.py
+++ pytzsub/sub.py
@@ -1,46 +1,65 @@
-import os
-from .fs import _read_from
+# MIT License
 
-__all__ = ['sub_timezone', 'all_code']
-DB_FILENAME = os.path.join(os.path.dirname(__file__), 'zone-sub.tab')
-ISOCODE_INDEX = _read_from(DB_FILENAME)
-DEFAUIL = {'tz': None}
+# Copyright (c) 2020 Maxim Barabash
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import os
+
+from .fs import read_tab
+
+__all__ = ("sub_timezone", "all_code")
+
+DB_FILENAME = os.path.join(os.path.dirname(__file__), "zone-sub.tab")
+ISOCODE_INDEX = read_tab(DB_FILENAME)
 
 
 def sub_timezone(isocode):
-    '''World timezone definitions by country subdivision
-    (e.g., provinces or states)
+    """World timezone definitions.
 
-    >>> from pytzsub import sub_timezone
-    >>> sub_timezone('US-CA')
-    'America/Los_Angeles'
-    >>>
-    '''
-    return ISOCODE_INDEX.get(isocode, DEFAUIL)['tz']
+    By country subdivision (e.g., provinces or states)
+
+    Args:
+        isocode (str): iso_code of country, provinces, states, e.g.
+
+    Returns:
+        (str, None): timezone name or None
+
+    Example:
+        >>> from pytzsub import sub_timezone
+        >>> sub_timezone("US-CA")
+        'America/Los_Angeles'
+        >>> sub_timezone("FOO") == None
+        True
+    """
+    return ISOCODE_INDEX.get(isocode, {"tz": None})["tz"]
 
 
 def all_code():
-    '''Returns all known codes of countries and subdivision
+    """Return all known iso_code of countries and subdivision.
 
-    >>> from pytzsub import all_code
-    >>> 'US-CA' in all_code()
-    True
-    >>> 'XXX' in all_code()
-    False
-    '''
+    Example:
+        >>> from pytzsub import all_code
+        >>> "US-CA" in all_code()
+        True
+        >>> "FOO" in all_code()
+        False
+    """
     return tuple(ISOCODE_INDEX.keys())
-
-
-# if __name__ == "__main__":
-#     import pytz
-#     for isocode in sorted(ISOCODE_INDEX.keys()):
-#         tz = sub_timezone(isocode)
-#         if tz not in pytz.common_timezones:
-#             print('#!!! not in common_timezones', isocode)
-#         print("%s\t%s" % (isocode, tz))
-
-#     country_names = set(pytz.country_names.keys())
-#     know_country_names = set([
-#         x for x in all_code() if '-' not in x
-#     ])
-#     print("# not set country_names:", country_names ^ know_country_names)
